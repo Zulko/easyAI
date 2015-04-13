2bc43c765fb285a33da135c3f68d57453ecbfcae
mrfesol <tom.wesolowski92@gmail.com>
2015-04-14 00:40:40 +0200

    DUAL algorithm

--- easyAI/AI/__init__.py
+++ easyAI/AI/__init__.py
@@ -1,3 +1,6 @@
 from .Negamax import Negamax
 from .DictTT import DictTT
 from .solving import id_solve, df_solve
+from .MTdriver import mtd
+from .SSS import SSS
+from .DUAL import DUAL
--- easyAI/__init__.py
+++ easyAI/__init__.py
@@ -4,3 +4,5 @@ __all__ = ['TwoPlayersGame', 'Human_Player', 'AI_Player',
 from .TwoPlayersGame import TwoPlayersGame
 from .Player import Human_Player, AI_Player
 from .AI import Negamax, DictTT, id_solve, df_solve
+from .AI import mtd
+from .AI import SSS, DUAL
