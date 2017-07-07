16fcbc85f4006b97dfe3b20d4dfeb774fecc6b5a
John Dupuy <johnd@makerredux.com>
2017-07-07 13:22:34 -0500

    fixed problem with PyPI dist; had to bump version

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
-import ez_setup
-ez_setup.use_setuptools()
+from setuptools import setup, find_packages
 
-from setuptools import setup, find_packages, Extension
-
-setup( name = 'easyAI',
-       author = 'Zulko 2013 and contributors',
-       version = '1.0.0.2',
-       description = 'Easy-to-use game AI algorithms (Negamax etc. )',
-       long_description=open('README.rst').read(),
-       license='LICENSE.txt',
-       keywords="board games AI artificial intelligence negamax",
-       packages= find_packages(exclude='docs'))
+setup(
+    name='easyAI',
+    version='1.0.0.4',
+    description='Easy-to-use game AI algorithms (Negamax etc. )',
+    long_description=open('README.rst').read(),
+    license='LICENSE.txt',
+    keywords="board games AI artificial intelligence negamax",
+    packages=find_packages(exclude='docs')
+)
